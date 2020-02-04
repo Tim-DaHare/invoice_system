ad9210eaa11f37d7e54f63a825c732a4f8665714
Tim de Haas <tim.de.haas@hotmail.nl>
2020-02-04 19:17:54 +0100

    added electron builder

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+const path = require("path")
 const { app, BrowserWindow } = require("electron")      
 
 let win
 
+console.log(app.isPackaged)
+
 const createWindow = () => {
+    // Todo fix cors and enable websecurity again
     win = new BrowserWindow({
         // width: 800, 
         // height: 600, 
@@ -12,13 +16,16 @@ const createWindow = () => {
     })
 
     // Todo: only load extensions for prod
-    // console.log(process.env.NODE_ENV)
-    // if (__DEV__) {
-    BrowserWindow.addDevToolsExtension("C:\\Users\\Tim de Haas\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\lmhkpmbekcpmknklioeibfkpmmfibljd\\2.17.0_0")
-    // }
+    if (!app.isPackaged) {
+        BrowserWindow.addDevToolsExtension("C:\\Users\\Tim de Haas\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\lmhkpmbekcpmknklioeibfkpmmfibljd\\2.17.0_0")
+    }
 
     // Todo: prod condition loadfile index.html
-    win.loadURL("http://localhost:3000/")
+    if (app.isPackaged) {
+        win.loadURL(`file://${path.join(__dirname, "../build/index.html")}`)
+    } else {
+        win.loadURL("http://localhost:3000/")
+    }
 }
       
 app.on("ready", createWindow)
